4229f908350e986e82252dffa5738e03ce55d538
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-01-20 15:11:43 -0500

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for(var i = 0; i < 25; i++) {
+    array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`)
+  }
+
+  return array
+}
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(n--)
+  }
+
+  return 'done'
+}
+
+function doWhileLoop(array) {
+  i = array.length - 1
+
+  do {
+    array.pop()
+    i--
+  } while(array[i] != null)
+
+  return array
+}
